20cfbbcae2f2af2899262b205bd547ae1a9a45e0
ComradDoge <34968288+ComradDoge@users.noreply.github.com>
2022-01-31 23:09:43 +0000

    compatability garbage

--- common/defines/NDefines.NCountry.lua
+++ common/defines/NDefines.NCountry.lua
@@ -1,18 +1,18 @@
 ----- DOGE NDefines.NDiplomacy -----
 
 -- For Ideas Expanded
-NDefines.NCountry.PS_BUY_IDEA = 300
-NDefines.NCountry.FREE_IDEA_GROUP_COST  = 7
-NDefines.NCountry.IDEA_TO_TECH = -0.01
-NDefines.NCountry.MAX_ACTIVE_POLICIES = 12
-NDefines.NCountry.BASE_POSSIBLE_POLICIES = 4
-NDefines.NCountry.MINIMUM_POLICY_TIME = 2
-NDefines.NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 0.35 --35
-
--- Innovativeness
-NDefines.NCountry.INNOVATIVENESS_FIRST_PICKED_IDEA = 1 -- 2.0
-NDefines.NCountry.INNOVATIVENESS_DAYS_AFTER_FIRST_PICK_VIABLE = 60 --90
-NDefines.NCountry.INNOVATIVENESS_FIRST_REACHED_TECH_LEVEL = 2 -- 4.0
+--NDefines.NCountry.PS_BUY_IDEA = 300
+--NDefines.NCountry.FREE_IDEA_GROUP_COST  = 7
+--NDefines.NCountry.IDEA_TO_TECH = -0.01
+--NDefines.NCountry.MAX_ACTIVE_POLICIES = 12
+--NDefines.NCountry.BASE_POSSIBLE_POLICIES = 4
+--NDefines.NCountry.MINIMUM_POLICY_TIME = 2
+--NDefines.NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 0.35 --35
+--
+---- Innovativeness
+--NDefines.NCountry.INNOVATIVENESS_FIRST_PICKED_IDEA = 1 -- 2.0
+--NDefines.NCountry.INNOVATIVENESS_DAYS_AFTER_FIRST_PICK_VIABLE = 60 --90
+--NDefines.NCountry.INNOVATIVENESS_FIRST_REACHED_TECH_LEVEL = 2 -- 4.0
 
 -- Military
 --NDefines.NCountry.LAND_FORCELIMIT_EXTRA_COST_FACTOR = 6.0
